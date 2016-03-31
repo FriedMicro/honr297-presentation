79a79db9176b75a9cbb6af9eb03c3346ee7b6960
Aidan Feay <aidanlfeay@gmail.com>
2016-03-31 15:57:09 -0400

    begin rendering quiz and styles

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,11 +1,24 @@
 //= require_tree .
 
-var Quiz = function (filepath) {
+var Quiz = function (container, filepath) {
+  var self = this;
+  self.container = container;
 
   $.getJSON(filepath, function(response){
     console.log("loading " + filepath);
-    console.log(JSON.stringify(response));
-   });
+  }).done(function(response) {
+    loadJSON(response);
+    loadQuiz();
+  });
+
+  var loadJSON = function(response) {
+    self.json = response;
+  }
+
+  var loadQuiz = function() {
+    $(self.container).css('display','block');
+    $(self.container).html('<p>' + self.json.title + '</p>');
+  }
 
 };
 
