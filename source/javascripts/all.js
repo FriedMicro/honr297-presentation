d93b1b0af288645efec874d31d78c135fad121b2
Aidan Feay <aidanlfeay@gmail.com>
2016-03-31 18:27:46 -0400

    more quiz work

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -17,9 +17,24 @@ var Quiz = function (container, filepath) {
 
   var loadQuiz = function() {
     $(self.container).css('display','block');
-    $(self.container).html('<p>' + self.json.title + '</p>');
-  }
+    var outputHTML;
+    outputHTML += '<p class="quizTitle">' + self.json.title + '</p>';
+    $.each(self.json.questions, function(index, question) {
+      outputHTML += '<div class="questionWrapper" data-qindex="' + index + '">';
+      outputHTML += '<p class="quizQuestion">' + question.question_name + '</p>';
+      $.each(question.answers, function(index, answer) {
+        outputHTML += '<p class="quizAnswer">' + (index+1) + ". " + answer + '</p>';
+      });
+      outputHTML += '</div>';
+    });
 
+    $(self.container).html(outputHTML);
+
+    $('.quizAnswer').click(function() {
+      var qindex = $(this).parent('.questionWrapper').html();
+      console.log(qindex);
+    });
+  }
 };
 
 (function() {
