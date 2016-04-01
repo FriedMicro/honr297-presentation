74846c1cbc85e2c582c1ef7f543eb64a18e75a94
Aidan Feay <aidanlfeay@gmail.com>
2016-03-31 21:13:36 -0400

    finished quiz engine

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -17,13 +17,13 @@ var Quiz = function (container, filepath) {
 
   var loadQuiz = function() {
     $(self.container).css('display','block');
-    var outputHTML;
-    outputHTML += '<p class="quizTitle">' + self.json.title + '</p>';
+    var outputHTML = "";
+    outputHTML += '<h2 class="quizTitle">' + self.json.title + '</h2>';
     $.each(self.json.questions, function(index, question) {
       outputHTML += '<div class="questionWrapper" data-qindex="' + index + '">';
-      outputHTML += '<p class="quizQuestion">' + question.question_name + '</p>';
+      outputHTML += '<h4 class="quizQuestion">' + question.question_name + '</h4>';
       $.each(question.answers, function(index, answer) {
-        outputHTML += '<p class="quizAnswer">' + (index+1) + ". " + answer + '</p>';
+        outputHTML += '<p class="quizAnswer" data-value="' + answer + '">' + (index+1) + ". " + answer + '</p>';
       });
       outputHTML += '</div>';
     });
@@ -31,8 +31,13 @@ var Quiz = function (container, filepath) {
     $(self.container).html(outputHTML);
 
     $('.quizAnswer').click(function() {
-      var qindex = $(this).parent('.questionWrapper').html();
-      console.log(qindex);
+      $(this).siblings().removeClass('correct incorrect');
+      var qindex = $(this).parent('.questionWrapper').data('qindex');
+      if ($(this).data('value') == self.json.questions[qindex].correct_answer) {
+        $(this).addClass('correct');
+      } else {
+        $(this).addClass('incorrect');
+      }
     });
   }
 };
