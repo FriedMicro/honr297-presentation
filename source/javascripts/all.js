52e14476c35422f9601f050ed9e47357a7183bce
EPICmynamesBG <mynamesbg@gmail.com>
2016-04-10 18:45:38 -0400

    Completed carousel

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -71,8 +71,8 @@ var Quiz = function (container, filepath) {
 $(function(){
     $('.slider .item:gt(0)').hide();
     setInterval(function(){
-      $('.slider .item:first-child').hide()
-         .next('.item').fadeIn()
+      $('.slider .item:first-child').fadeOut(700)
+         .next('.item').fadeIn(700)
          .end().appendTo('.slider');},
       5000);
 });
